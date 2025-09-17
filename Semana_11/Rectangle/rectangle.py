e2dd16606860cbc6472eae7fd11e67c71e046cbc
MarBadillaCampos <marbadilla97@gmail.com>
2025-09-16 22:31:15 -0600

    Some validations were added it, to the rectangle class

--- Semana_11/Rectangle/rectangle.py
+++ Semana_11/Rectangle/rectangle.py
@@ -9,20 +9,55 @@ class Rectangle:
     
     def get_perimeter(self):
         return (2*(self.width + self.height))
-      
+class Menu:
+    def __init__(self):
+        pass
 
-width = int(input('Enter the width value: '))
-height = int(input('Enter the height value: '))
+    def ask_width(self):
+        while True:
+            try:
+                width = int(input(f'Enter the width value: '))
+                if 0 <= width:
+                    return width
+                else:
+                    print(f'Value must positive Please try again')
+                    
+            except ValueError:
+                print("Invalid input. Please enter a numeric value. Please try again")
+    
+    def ask_height(self):
+        while True:
+            try:
+                height = int(input(f'Enter the height value: '))
+                if 0 <= height:
+                    return height
+                else:
+                    print(f'Value must positive Please try again')
+                    
+            except ValueError:
+                print("Invalid input. Please enter a numeric value. Please try again")
 
-if width > 0 < height:
-    rectangle = Rectangle(width,height)
-    rectangle_area = rectangle.get_area()
-    rectangle_perimeter = rectangle.get_perimeter()
+class Main:
+    def __init__(self):
+        pass
 
-    print(f'Area: {rectangle_area}')
-    print(f'Perimeter: {rectangle_perimeter}')
-else:
-    print('Invalid Number, negative values are not allowed')
+    def action(self):
+        menu_handler = Menu()
+        width = menu_handler.ask_width()
+        height = menu_handler.ask_height()
+        rectangle = Rectangle(width,height)
+        rectangle_area = rectangle.get_area()
+        rectangle_perimeter = rectangle.get_perimeter()
+
+        print(f'Area: {rectangle_area}')
+        print(f'Perimeter: {rectangle_perimeter}')
+
+                
+
+main = Main()
+main.action()
+
+
 
             
 
