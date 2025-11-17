d63eeda4edbf9ef594738892ac7fbd7edf6f1143
MarBadillaCampos <marbadilla97@gmail.com>
2025-11-17 14:05:49 -0600

    bubble sort steps file

---
+++ Semana_15/04_bubble_sort_steps.py
@@ -0,0 +1,22 @@
+
+def bubble_sort(sort_list):
+    cont = 0
+    swap = 0
+    for out_index in range(0, len(sort_list) -1 ):
+        cont = cont + 1
+        for index in range(0, len(sort_list) - 1 - out_index):
+            current_element = sort_list[index]
+            next_element = sort_list[index + 1]
+            print(f'-- Iteration {index}. Current Element: {current_element}, Next Element: {next_element}')
+
+            if current_element > next_element:
+                print('Current Element is bigger than Next element. lets swapped it...')
+                swap = swap + 1
+                sort_list[index] = next_element
+                sort_list[index + 1] = current_element
+    print(f'Iterations: {cont}')
+    print(f'Swapped: {swap}')
+
+my_list = [5, 4, 3, 2, 1]
+bubble_sort(my_list)
+print(my_list)
